5f33fd945ff90e01e111c75e70abb8709c79ebf8
Atif Rehan <rockrehan7077@gmail.com>
2026-01-20 14:30:06 +0530

    created a route for product list

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,15 +2,34 @@ const express = require("express");
 const Product = require("../models/product");
 const router = express.Router();
 
-router.get("/", async (req, res)=>{
-    try{
-        const products = await Product.find()
-        res.json(products)
-    }catch(err){
-        res.status(500).json({
-            message:"failed to fetch products"
-        })
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({
+      message: "failed to fetch products",
+    });
+  }
+});
+router.get("/products", async (req, res) => {
+  try {
+    const { search } = req.query;
+    if (typeof search !== "string") {
+      search = "";
     }
+    const filter = {};
+
+    if (search) {
+      filter.title = { $regex: search, $options: "i" };
+    }
+    const products = awaitProduct.find({ filter });
+    res.status(200).json(products);
+  } catch (err) {
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
-module.exports = router
+module.exports = router;
