450aca086999f7a854196adcd03bcd7e15712de0
Atif Rehan <rockrehan7077@gmail.com>
2026-02-09 07:28:18 +0530

    Added a route for specific product

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,32 +2,42 @@ const express = require("express");
 const Product = require("../models/product");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({
-      message: "failed to fetch products",
-    });
-  }
-});
 router.get("/products", async (req, res) => {
   try {
-    const { search } = req.query;
+    let { search } = req.query;
+
     if (typeof search !== "string") {
       search = "";
     }
+
     const filter = {};
 
     if (search) {
       filter.title = { $regex: search, $options: "i" };
     }
-    const products = awaitProduct.find({ filter });
+
+    const products = await Product.find(filter);
     res.status(200).json(products);
-  } catch (err) {
-    return res.status(500).json({
+  } catch (error) {
+    res.status(500).json({
       message: "Server error",
+    });
+  }
+});
+
+router.get("/products/:id", async (req, res) => {
+  try {
+    if (!req.params.id) {
+      return res.status(400).json({
+        message: "Missing required paramter: 'id'",
+      });
+    }
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
     });
   }
 });
