41a5bedf07e7dec5c8002921a19ae77dc23516f2
Atif Rehan <rockrehan7077@gmail.com>
2026-03-08 15:39:51 +0530

    added a home section at navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router'
 import './Navbar.css'
 export const Navbar = () => {
    return( <nav>
         <h2 className='nav-logo'>SnapShop</h2>
         <ul className="nav-menu">
+            <Link to='/' className='nav-menu primary'>Home</Link>
             <li className="nav-menu primary">Categories</li>
             <li className="nav-menu primary">Deals</li>
             <li className="nav-menu primary">About</li>
