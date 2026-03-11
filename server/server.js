bd28d51f5361b55af0c52c3d92f7f1ad567d7cf4
Atif Rehan <rockrehan7077@gmail.com>
2026-03-12 00:25:49 +0530

    Add error handling and middleware ordering to server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,17 @@ const app = express();
 
 connectDB();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api',require('./routes/product.route.js'))
+app.use("/api", require("./routes/product.route.js"));
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
