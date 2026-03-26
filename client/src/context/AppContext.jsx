c34c02024c4f687a6ceabd453ca15c1c9a8486a0
Atif Rehan <rockrehan7077@gmail.com>
2026-03-26 12:16:22 +0530

    add error and loading state

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+export const AppContext = createContext()
+
+export const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const showLoader = () => setLoading(true);
+    const hideLoader = () => setLoading(false);
+    const showError = (msg) => setError(msg);
+    const hideError = () => setError(null)
+
+
+
+    return (
+        <AppContext.Provider value={{
+            loading,
+            error,
+            showLoader,
+            hideLoader,
+            hideError
+        }}>
+            {children}
+        </AppContext.Provider>)
+}
+export const useApp = useContext(AppContext)
