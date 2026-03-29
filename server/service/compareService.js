67f29b93fa4b0ac4a76f577c1e0040ebdbd7f0d5
Atif Rehan <rockrehan7077@gmail.com>
2026-03-29 13:58:30 +0530

    update Flipkart scraper logic

--- server/service/compareService.js
+++ server/service/compareService.js
@@ -1,11 +1,12 @@
 const amazonScraper = require("../scraper/amazon");
-const flipkartScraper = require("../scraper/flipkart ");
+const flipkartScraper = require("../scraper/flipkart");
 
 async function compareService(product) {
   const [amazonData, flipkartData] = await Promise.all([
     amazonScraper(product),
     flipkartScraper(product),
   ]);
+ 
   return { amazon: amazonData, flipkart: flipkartData };
 }
-module.exports = compareService;
+module.exports = compareService; 
