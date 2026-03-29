e5b64885f54ce9897a32dfdd39fef3d8479d2f1f
Atif Rehan <rockrehan7077@gmail.com>
2026-03-29 15:09:27 +0530

    update Flipkart scraper logic

--- server/scraper/flipkart.js
+++ server/scraper/flipkart.js
@@ -1,25 +1,35 @@
 const puppeteer = require("puppeteer");
 async function scrapeFlipkartList(searchTerm) {
-  const browser = await puppeteer.launch({ headless: "new"});
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   await page.goto(`https://www.flipkart.com/search?q=${searchTerm}`);
   await page.waitForSelector(".lvJbLV");
 
-  const products = await page.evaluate(()=>{
-    const items = document.querySelectorAll(".lvJbLV")
+  const products = await page.evaluate(() => {
+    const items = document.querySelectorAll(".lvJbLV");
     return [...items]
       .map((item) => ({
-        title: item.querySelector(".RG5Slk")?.innerText,
+        title:
+          item.querySelector(".RG5Slk")?.innerText ||
+          item.querySelector(".pIpigb")?.innerText,
+
         price: item.querySelector(".hZ3P6w")?.innerText,
+
         rating: item.querySelector(".MKiFS6")?.innerText,
-        url: item.querySelector(".k7wcnx")?.href,
-        image: item.querySelector(".lWX0_T img")?.src,
+
+        url:
+          item.querySelector(".k7wcnx")?.href ||
+          item.querySelector(".pIpigb")?.closest("a")?.href,
+
+        image:
+          item.querySelector(".lWX0_T img")?.src ||
+          item.querySelector(".UCc1lI")?.src,
       }))
       .filter((p) => p.title);
-  })
-  await browser.close()
-  return products
-  
+  });
+  await browser.close();
+  return products;
 }
-// scrapeFlipkartList('iphone').then(console.log)
+
+module.exports = scrapeFlipkartList;
