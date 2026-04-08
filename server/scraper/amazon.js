7d9b8ea11c3088fb0d052f2e956e4710bfe2f2f1
Atif Rehan <rockrehan7077@gmail.com>
2026-04-08 16:10:12 +0530

    correct selector for title for product title excrator

--- server/scraper/amazon.js
+++ server/scraper/amazon.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function scrapeAmazonList(searchTerm) {
-  const browser = await puppeteer.launch({ headless: "new"});
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   await page.goto(`https://www.amazon.in/s?k=${searchTerm}`);
@@ -11,7 +11,7 @@ async function scrapeAmazonList(searchTerm) {
     const items = document.querySelectorAll(".s-result-item[data-asin]");
     return [...items]
       .map((item) => ({
-        title: item.querySelector("h2 span")?.innerText,
+        title: item.querySelector("a h2 span")?.innerText,
         price: item.querySelector(".a-price-whole")?.innerText,
         rating: item.querySelector(".a-icon-alt")?.innerText,
         url: item.querySelector("h2 a")?.href,
