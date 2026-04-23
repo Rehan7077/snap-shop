4c7a8a141bb204abcf263e498f199293d150d3d9
Atif Rehan <rockrehan7077@gmail.com>
2026-04-23 18:20:50 +0530

    refactor: reorganize folder structure

--- server/service/compareService.js
+++ server/service/compareService.js
@@ -1,5 +1,5 @@
-const amazonScraper = require("../scraper/amazon");
-const flipkartScraper = require("../scraper/flipkart");
+const amazonScraper = require("../scraper/amazon/amazon");
+const flipkartScraper = require("../scraper/flipkart/flipkart");
 
 async function compareService(product) {
   const [amazonData, flipkartData] = await Promise.all([
